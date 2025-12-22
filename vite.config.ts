7825bf84d4e0e4c46b1a0ec066784d4bb1fbf5c0
MilapsKhasi <khasimilap@gmail.com>
2025-12-22 22:21:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
-import path from 'path';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'Vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-export default defineConfig
-      plugins: [react()],
+export default ddefineConfig{{
+plugins: [react()],
       resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+      alias: {
+            '@':path.resolve(_dirname,'.'),
+      },
+    }.
+ })
