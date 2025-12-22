c96dabcb965576d68017f94834d87172cb27fa46
MilapsKhasi <khasimilap@gmail.com>
2025-12-22 18:10:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 document.getElementById("root").innerHTML = 
-  <h1 style="color:white;
-    font-size:30px">
+  '<h1 style="color:white; font-size:30px">
     Purchase Master Loaded
-    </h1>
+    </h1>'
 ;
