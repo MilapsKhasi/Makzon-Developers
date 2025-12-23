615f79a5acc2ce48f39148f971466f366e68f7a3
MilapsKhasi <khasimilap@gmail.com>
2025-12-23 16:15:16 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from 'Vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+const _filename = fileURLToPath(import.meta.url)
+const _dirname = path.dirname(__filename)
 
 export default defineConfig({
 plugins: [react()],
+resolve: {
+  alias: {
+      '@': path.resolve(__dirname, '/src'),
+    },
+  },
 }) 
