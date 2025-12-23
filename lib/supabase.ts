fc6358a0ed776bb50e181f03936cbea5a15ce1b3
MilapsKhasi <khasimilap@gmail.com>
2025-12-23 17:46:13 +0530

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,5 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://siwceltyyhswddnlaibc.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpd2NlbHR5eWhzd2RkbmxhaWJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYzMTA2OTAsImV4cCI6MjA4MTg4NjY5MH0.uRhNoXhxjHAp7C9oZZrW0o4ZhzNxRiISvNeIj5WX1RE';
+import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
