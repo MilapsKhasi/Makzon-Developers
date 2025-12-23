ba0721bda850d977a6586df6eae504279ef2fb85
MilapsKhasi <khasimilap@gmail.com>
2025-12-23 16:45:21 +0530

    Update and rename vite.config.ts to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 import { fileURLToPath } from 'url'
 import path from 'path'
 
-const _filename = fileURLToPath(import.meta.url)
 const _dirname = path.dirname(__filename)
+const _filename = fileURLToPath(import.meta.url)
 
 export default defineConfig({
 plugins: [react()],
