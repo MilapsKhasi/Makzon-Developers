1f196d1acc237a7dc3755973d690df6d09dbac31
MilapsKhasi <khasimilap@gmail.com>
2026-01-21 10:00:14 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
-
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/react-refresh';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
-  server: {
-    port: 3000
+    chunkSizeWarningLimit: 1000, // Raises the limit to 1000kb
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
   }
 });
