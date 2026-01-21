7147ba03c2061db502c07f0455422675e52362d9
MilapsKhasi <khasimilap@gmail.com>
2026-01-21 10:11:47 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/react-refresh';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000, // Raises the limit to 1000kb
+    chunkSizeWarningLimit: 1600, // Increases the limit to stop the warning
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
+            // This splits large libraries into their own files
             return id.toString().split('node_modules/')[1].split('/')[0].toString();
           }
-        }
-      }
-    }
-  }
+        },
+      },
+    },
+  },
 });
